8c9d93752946eb16892b00f526252795efe3cab2
Toacin <toacinp@outlook.com>
2022-09-16 14:51:29 -0700

    added comments to javascript file

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// library of characters created here to support password generator
 const lowerAlph = ['a','b','c','d','e','f','d','e','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 const upperAlph = ['A','B','C','D','E','F','D','E','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 const num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-const special = ['!','@','#','$','%','^','&','*'];
+const special = ['!','@','#','$','%','^','&','*', ')', '(', '_', '+', '-', '<', '>'];
+// initialized an array that holds all the arrays
 const criteria = [lowerAlph, upperAlph, num, special];
 
-
+// function code below
 function generatePassword() {
-  length = prompt('What should the length of the password be: ');
+  // this prompts the user to choose a length and stores it in a length variable
+  let length = prompt('What should the length of the password be: ');
+
+  // this checks in user inputted length is valid. If so, it will asks a series of confirms on whether or not they want certain criteria met, stored in new variables.
   if (length<8 || length>128) {
     alert("Please enter a length between 8 and 128 characters.");
     generatePassword();
@@ -19,17 +25,25 @@ function generatePassword() {
     specialChar = confirm('Should it include special symbols');
   }
   
-  let criteriaSelect = [lowercase, uppercase, numeric, specialChar];
+  // this initializes an array of all potential user choices
+  const criteriaSelect = [lowercase, uppercase, numeric, specialChar];
+  // this initializes an empty array which will eventually be reassigned with concacted arrays that hold characters based on user choice
   let newCriteria = [];
 
+  // this for loops checks boolean values for each criteriaSelect element, and concatenates corresponding criteria arrays to newCriteria array
   for (i=0; i<criteriaSelect.length; i++){
     if (criteriaSelect[i]) {
-    newCriteria = newCriteria.concat(criteria[i]);}
+      newCriteria = newCriteria.concat(criteria[i]);
+    }
   }
-
+  
+  //this is just to check if newCriteria properly holds only characters based on character selection
   console.log(newCriteria);
 
-  passw = ""
+  //initialize passw to hold a generated password only using characters from newCriteria Array
+  let passw = ""
+
+  //this for loop will create a choose a new character from newCriteria array and string concat. to passw variable. It will run as many times as the user inputted length.
   for (i=0; i < length; i++){
     newChar = newCriteria[Math.floor(Math.random()*newCriteria.length)];
     passw = passw.concat(newChar);
